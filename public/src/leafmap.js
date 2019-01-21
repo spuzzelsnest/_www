9050084debfd4dede476b0da31a15408f7c418db
spuzzelsnest <jan.mpdesmet@gmail.com>
2019-01-21 23:53:23 +0100

    added public dir with POI site based on Leaflet

---
+++ public/src/leafmap.js
@@ -0,0 +1,26 @@
+function loadMap(){
+
+	var map = L.map('map').setView([47.9, 12], 6);
+
+	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}).addTo(map);
+        var LeafIcon = L.Icon.extend({
+                options: {
+                        iconSize:     [20, 20]
+              }
+        });
+
+	 var iconType = {};
+                iconType['1'] = new LeafIcon({iconUrl: '/img/Afoto.png'});
+                iconType['2'] = new LeafIcon({iconUrl: '/img/Xfoto.png'});
+                iconType['3'] = new LeafIcon({iconUrl: '/img/Avideo.png'});
+                iconType['4'] = new LeafIcon({iconUrl: '/img/Xvideo.png'});
+         
+	for(var i in markers){
+     		var lat = markers[i].lat;
+     		var lng = markers[i].lng;
+		var dif  = markers[i].icon;
+		var title = markers[i].title;
+	
+		L.marker([lat, lng], {icon: iconType[dif]}).bindPopup(title).addTo(map);
+	}
+}
