4d3a298d9b2066a10ed890a8c12d0eac09922d8e
spuzzelsnest <jan.mpdesmet@gmail.com>
2019-01-22 22:15:08 +0100

    added dif for pix vs vids

--- public/src/leafmap.js
+++ public/src/leafmap.js
@@ -1,6 +1,6 @@
 function loadMap(){
 
-	var map = L.map('map').setView([51.9, 12], 5);
+	var map = L.map('map').setView([50.1, 8], 6);
 
 	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}).addTo(map);
         var LeafIcon = L.Icon.extend({
@@ -27,13 +27,35 @@ function loadMap(){
 				 opacity: 0.5
 		}
 	});
+
+	var customOptions ={
+	        'maxWidth': '500',
+		'overflow-y': 'scroll',
+       		'className' : 'custom'
+        }
+
 	for(var i in markers){
      		var lat = markers[i].lat;
      		var lng = markers[i].lng;
 		var dif  = markers[i].typeId;
-		var title = markers[i].shortdesc;
 
-		var marker = L.marker([lat, lng], {icon: iconType[dif]}).bindPopup(title);
+		var title = markers[i].shortdesc;
+		var img = markers[i].name+'.jpg';
+		var place = markers[i].place;
+		var country = markers[i].country;
+		var date = markers[i].date;
+		var info = markers[i].info;
+		
+		if (dif < 2){
+
+			var customPopup = "<b><u>"+title+" "+place+" ("+country+")</u></b><br/><center><img src='/images/" + img + ".jpg' alt='' width='350px'/></center><br>"+ info;
+		}else{
+	
+			var customPopup = "<b><u>"+title+" "+ place+" ("+country+")</u></b><br><center><video id=\""+img+"\" poster=\"media/"+img+"/"+img+".jpg\" width=\"480\" height=\"360\" controls=\"autoplay\"><source src=\"media/"+img+"/"+img+".mp4\" type=\"video/mp4\"><source src=\"media/"+img+"/"+img+".ogg\" type=\"video/ogg\"></center><br>"+ info;
+		}
+
+
+		var marker = L.marker([lat, lng], {icon: iconType[dif]}).bindPopup(customPopup,customOptions);
 		cluster.addLayer(marker);
 	}
 	map.addLayer(cluster);
