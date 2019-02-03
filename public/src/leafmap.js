324821c6916a43d64231c911536a562c6b1d9251
spuzzelsnest <jan.mpdesmet@gmail.com>
2019-02-03 23:40:33 +0100

    split into div

--- public/src/leafmap.js
+++ public/src/leafmap.js
@@ -1,4 +1,4 @@
-function loadMap(){
+function loadMap() {
 
 	var map = L.map('map').setView([50.1, 6], 6);
 
@@ -10,13 +10,15 @@ function loadMap(){
         });
 
 	 var iconType = {};
-                iconType['1'] = new LeafIcon({iconUrl: '/img/Afoto.png'});
+
+    iconType['1'] = new LeafIcon({iconUrl: '/img/Afoto.png'});
                 iconType['2'] = new LeafIcon({iconUrl: '/img/Xfoto.png'});
                 iconType['3'] = new LeafIcon({iconUrl: '/img/Avideo.png'});
                 iconType['4'] = new LeafIcon({iconUrl: '/img/XVideo.png'});
 
 	var cluster = L.markerClusterGroup({
-		spiderfyOnMaxZoom: true,
+
+        spiderfyOnMaxZoom: true,
 		showCoverageOnHover: false,
 		zoomToBoundsOnClick: true,
 		removeOutsideVisibleBounds:true,
@@ -28,44 +30,36 @@ function loadMap(){
 		}
 	});
 
-	var customOptions ={
-	        'maxWidth': '600',
-       		'className' : 'mapPopUp'
-        }
-
 	for(var i in markers){
-     		var lat = markers[i].lat;
-     		var lng = markers[i].lng;
-		var dif = markers[i].typeId;
 
+        var lat = markers[i].lat;
+        var lng = markers[i].lng;
+		var dif = markers[i].typeId;
 		var title = markers[i].shortdesc;
 		var img = markers[i].name;
 		var place = markers[i].place;
 		var country = markers[i].country;
 		var date = markers[i].date;
 		var info = markers[i].info;
-		
+        
 		if (dif < 3){
 
-			var customPopup = "<big><u>"+title+" "+place+" ("+country+")</u></big><br/><center><img src='/images/" + img + ".jpg' alt='' width='350px'/></center><br>"+ info;
+			var customPopup = "<big><u>"+title+" "+place+" ("+country+")</u></big><p><center><img src='/images/" + img + ".jpg' alt='' width='350px'/></center><br>"+ info;
 		}else{
 	
-			var customPopup = "<big><u>"+title+" "+ place+" ("+country+")</u></big><br><center><video id=\""+img+"\" poster=\"media/"+img+"/"+img+".jpg\" width=\"480\" height=\"360\" controls=\"autoplay\"><source src=\"media/"+img+"/"+img+".mp4\" type=\"video/mp4\"><source src=\"media/"+img+"/"+img+".ogg\" type=\"video/ogg\"></center><br>"+ info;
+			var customPopup = "<big><u>"+title+" "+place+" ("+country+")</u></big><p>    <center><video id=\""+img+"\" poster=\"media/"+img+"/"+img+".jpg\" width=\"480\" height=\"360\" controls=\"autoplay\"><source src=\"media/"+img+"/"+img+".mp4\" type=\"video/mp4\"><source src=\"media/"+img+"/"+img+".ogg\" type=\"video/ogg\"></center><br>"+ info;
 		}
 
 		var marker = L.marker([lat, lng], {icon: iconType[dif]});
-		marker.on('click', sideDiv);
+        marker.html = customPopup;
+        marker.on('click', sideDiv);
 		cluster.addLayer(marker);
-
 	}
 	map.addLayer(cluster);
-	
 }
 
-function sideDiv(e) {
-
-	var markerInfo = document.getElementById('markerInfo');
-
-	document.getElementById('markerInfo').textContent=this.getLatLng();
-
+function sideDiv(e){
+      
+	var text= this.html;
+    document.getElementById('markerInfo').innerHTML = text;   
 }
