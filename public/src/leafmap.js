a1f3669e6037adde09ec6564034eb845b63e9786
spuzzelsnest <j.mpdesmet@gmail.com>
2019-02-08 18:58:56 +0100

    filter data by checkbox

--- public/src/leafmap.js
+++ public/src/leafmap.js
@@ -4,39 +4,49 @@ function loadMap() {
         iconType['0'] = '/img/marker1.png';
         iconType['1'] = '/img/marker2.png';
 
-     var legName = {};
+    var legName = {};
         legName['0'] = 'unverified';
         legName['1'] = 'Verified';
-  
+    
     var cat = [];
-    for(i = 0; i < markers.length; i++){    
-        if(cat.indexOf(markers[i].Verified) === -1){
-        cat.push(markers[i].Verified);
+    var catData =[];
+    var data =[];
+    
+    for(i = 0; i < markers.length; i++){
+        if (cat.indexOf(markers[i].Verified) === -1){
+            
+            cat.push(markers[i].Verified);
+            console.log(cat.length);
         }
     }
     
     for(i = 0; i< cat.length; i++){
         
-        distCount = (jQuery.grep(markers,function(item, c){return(item.Verified == cat[i] && c > 1);})).length;
-        document.getElementById('legenda').innerHTML += "<img src="+iconType[cat[i]]+" height='20px' width='25px'> <input type='checkbox' class='leaflet-control-layers-selector' name='typeId' vaulue="+cat[i]+" checked onchange='loadingMap(markers);'/> "+distCount+" "+legName[cat[i]]+" · ";
+        catData = jQuery.grep(markers,function(item, c){return(item.Verified == cat[i] && c > 1);});
+        distCount = catData.length;
+        
+        document.getElementById('legenda').innerHTML += "<img src="+iconType[cat[i]]+" height='20px' width='25px'> <input type='checkbox' class='leaflet-control-layers-selector' name='typeId' value="+cat[i]+" checked/> "+distCount+" "+legName[cat[i]]+" · ";
+
+        data = catData.concat(data);
     }
 
-loadingMap(markers, iconType);
+    loadingMap(data, iconType);
 }
 
-function loadingMap(markers,iconType){
-
-	var map = L.map('map').setView([46.5, 9], 5);
-
-	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}).addTo(map);
-        var LeafIcon = L.Icon.extend({
-                options: {
-                    iconSize:[20, 25]
+function loadingMap(data, iconType){
+    console.log(data.length);
+    
+    var map = L.map('map').setView([46.5, 9], 5);
+    
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}).addTo(map);
+    
+    var LeafIcon = L.Icon.extend({
+        options: {
+                iconSize:[20, 25]
               }
         });
 
-	var cluster = L.markerClusterGroup({
-
+    var cluster = L.markerClusterGroup({
         spiderfyOnMaxZoom: true,
 		showCoverageOnHover: false,
 		zoomToBoundsOnClick: true,
@@ -49,20 +59,20 @@ function loadingMap(markers,iconType){
 		}
 	});
     
-	for(var i in markers){
+	for(var i in data){
 
-        var country = markers[i].Country;
-		var name = markers[i].Name;
-        var address = markers[i].Address;
-        var zcode = markers[i].Zip_Code;
-        var city = markers[i].City;
-        var phone = markers[i].Phone;
-		var email = markers[i].Email;
-        var web = markers[i].Website;
-        var contact = markers[i].Contact;
-        var lat = markers[i].Lat;
-        var lng = markers[i].Lng;
-		var dif = markers[i].Verified;
+        var country = data[i].Country;
+		var name = data[i].Name;
+        var address = data[i].Address;
+        var zcode = data[i].Zip_Code;
+        var city = data[i].City;
+        var phone = data[i].Phone;
+		var email = data[i].Email;
+        var web = data[i].Website;
+        var contact = data[i].Contact;
+        var lat = data[i].Lat;
+        var lng = data[i].Lng;
+		var dif = data[i].Verified;
         
         var title = name+" - "+city;
         var code = "<big><u>"+title+" ("+country+")</u></big><p><center><br>"+ address;
@@ -72,14 +82,15 @@ function loadingMap(markers,iconType){
         marker.on('click', sideDiv);
 		cluster.addLayer(marker);
 	}
-	map.addLayer(cluster);
+
+    map.addLayer(cluster);
     
     geojson = L.geoJson(ITcurrentRegions).addTo(map);
     geojson = L.geoJson(EUcurrentCountries).addTo(map);
     
     geojson.eachLayer(function (layer) {
         layer.bindPopup(layer.feature.properties.name);
-    });
+    });    
 }
 
 function sideDiv(e){
