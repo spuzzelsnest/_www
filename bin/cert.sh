d69017c69a5217cabe98d60baf0a4ec38dd6a408
spuzzelsnest <jdesmet@weedmaps.com>
2019-04-05 10:36:47 +0200

    permissions for https,key in bash

--- bin/cert.sh
+++ bin/cert.sh
@@ -6,3 +6,6 @@ openssl genrsa -des3 -out ../cert/https.key 2048
 openssl req -new -key ../cert/https.key -out ../cert/https.csr
 openssl x509 -req -days 365 -in ../cert/https.csr -signkey ../cert/https.key -out ../cert/https.crt
 
+
+chmod 755 ../cert
+chmod 700 ../cert/*.key
